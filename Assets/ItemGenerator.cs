c5547add36650a8f72fbfd8e967620c453c8d768
yuusukesawai <dom.spiro.spero.ykubw@ezweb.nejp>
2018-10-05 19:38:29 +0900

    Lesson6の課題提出です。 発展課題込です。

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+    //carPrefabを入れる
+    public GameObject carPrefab;
+    //coinPrefabを入れる
+    public GameObject coinPrefab;
+    //cornPrefabを入れる
+    public GameObject conePrefab;
+    //スタート地点
+    private int startPos = -160;
+    //ゴール地点
+    private int goalPos = 120;
+    //アイテムを出すx方向の範囲
+    private float PosRange = 3.4f;
+
+    private GameObject unitychan;
+    
+    // Use this for initialization
+    void Start()
+    {
+       this.unitychan = GameObject.Find("unitychan");
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (this.unitychan.transform.position.z >= startPos - 50 && startPos < goalPos)
+        {
+
+            
+                //どのアイテムを出すのかをランダムに設定
+                int num = Random.Range(1, 11);
+                if (num <= 2)
+                {
+                    //コーンをx軸方向に一直線に生成
+                    for (float j = -1; j <= 1; j += 0.4f)
+                    {
+                        GameObject cone = Instantiate(conePrefab) as GameObject;
+                        cone.transform.position = new Vector3(4 * j, cone.transform.position.y, startPos);
+                    }
+                
+            }
+                else
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        //アイテムの種類を決める
+                        int item = Random.Range(1, 11);
+                        //アイテムを置くz座標のオフセットをランダムに配置
+                        int offsetZ = Random.Range(-5, 6);
+                        //60％コイン配置、30%車配置、10%何もなし
+                        if (1 <= item && item <= 6)
+                        {
+                            //コインを生成
+                            GameObject coin = Instantiate(coinPrefab) as GameObject;
+                            coin.transform.position = new Vector3(PosRange * j, coin.transform.position.y, startPos + offsetZ);
+                        }
+                        else if (7 <= item && item <= 9)
+                        {
+                            //車を生成
+                            GameObject car = Instantiate(carPrefab) as GameObject;
+                            carPrefab.transform.position = new Vector3(PosRange * j, car.transform.position.y, startPos + offsetZ);
+                        }
+                    }
+                }
+            startPos += 15;
+            Debug.Log(this.unitychan.transform.position.z);
+        }
+    }
+}
---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour {
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（Z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+    }
+		
+	
+	// Update is called once per frame
+	void Update () {
+        //Unityちゃんの位置に合わせてカメラの位置も移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+		
+	}
+
+ 
+}
